af48293fefb561cb7630afc3797560c42d70cc6d
benav111 <benav111@mail.chapman.edu>
2015-10-11 21:02:17 -0700

    Project
    yep

---
+++ New Unity Project 1/Assets/ObjectCollection.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectCollection : MonoBehaviour {
+	//DONT LEAVE THIS VARIABLE HERE, MOVE WITH UPDATE INFO TO MAIN SCRIPT
+	bool endGame = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//PUT THIS IN MAIN SCRIPT, DONT LEAVE IN THIS ONE, ALSO TAKE THE VARIABLE ENDGAME 
+		//if there are no more collectables yet, this tag is arbitrary, then end the game
+		if (GameObject.FindGameObjectsWithTag ("Cube").Length == 0) 
+		{
+			endGame = true;
+		}
+		if (endGame == true)
+		{
+			//ENTER WHAT YOU WANT TO HAPPEN WHEN THE GAME ENDS HERE.
+		}
+	}
+	//if collides with player, destroy cube
+	void OnCollisionEnter (Collision col)
+	{	/*THE PLAYER MUST BE TAGGED AS SUCH	
+		THIS PREVENTS THE COLLECTABLE FROM ACCIDENTALLY BEING
+		DESTROYED BY OTHER OBJECTS*/
+		if(col.gameObject.name == "Player")
+		{
+			Destroy(this.gameObject);
+		}
+	}
---
+++ New Unity Project 1/Assets/Walking.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walking : MonoBehaviour {
+
+	// Use this for initialization
+	public float walkSpeed = 2.0f;
+	public float wallLeft = GameObject.Find("Enemy").transform.position.x- 2.0f;
+	public float wallRight = GameObject.Find("Enemy").transform.position.x+ 2.0f;
+	float walkingDirection = 1.0f;
+	Vector3 walkAmount;
+	// Update is called once per frame
+	void Update () {
+		walkAmount.x = walkingDirection * walkSpeed * Time.deltaTime;
+		if (walkingDirection>0.0f && transform.position.x >=wallRight)
+		{//switch directions	
+			walkingDirection = -1.0f;
+		}
+		else if (walkingDirection < 0.0f && transform.position.x <= wallLeft)
+		{//switch directions
+			walkingDirection = 1.0f;
+		}
+		transform.Translate(walkAmount);
+	}
+}
